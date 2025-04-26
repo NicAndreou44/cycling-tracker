d104a448c35fe40bb95fa423059a0e03c0dcb569
Nic Andreou <Nic.Andreou1995@gmail.com>
2025-04-26 17:44:08 +0200

    Complete backend with auth, Postgres, JWT, bcrypt, jesting, loving this

--- backend/src/middleware/authmiddleware.ts
+++ backend/src/middleware/authmiddleware.ts
@@ -2,11 +2,10 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 import jwt from "jsonwebtoken";
 
 export interface AuthRequest extends Request {
-  user: { userId: number };
+  user?: { userId: number }; 
 }
 
-
-const SECRET = "dev_secret";
+const SECRET = process.env.JWT_SECRET || "dev_secret"; 
 console.log("SECRET USED TO VERIFY TOKEN:", SECRET);
 
 export const authenticateJWT: RequestHandler = (req, res, next) => {
@@ -17,14 +16,18 @@ export const authenticateJWT: RequestHandler = (req, res, next) => {
   }
 
   const token = authHeader.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ error: "Token format invalid" });
+    return;
+  }
+
   try {
     const payload = jwt.verify(token, SECRET) as { userId: number };
     (req as AuthRequest).user = { userId: payload.userId };
     next();
-  } catch (error) { 
+  } catch (error) {
     console.error("JWT VERIFY ERROR:", error);
     res.status(403).json({ error: "Invalid or expired token" });
     return;
   }
 };
-
